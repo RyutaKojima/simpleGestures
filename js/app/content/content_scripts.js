3b62159a6b5a08cc192f5945bec81fc9d5f54139
R.K <rkojima0213@gmail.com>
2018-08-21 18:41:32 +0900

    style

--- js/app/content/content_scripts.js
+++ js/app/content/content_scripts.js
@@ -17,7 +17,7 @@ const ContentScripts = function (trailCanvas) {
 ContentScripts.prototype.loadOption = function () {
 	const _this = this;
 
-	chrome.extension.sendMessage({msg: "load_options"}, function(response) {
+	chrome.extension.sendMessage({msg: 'load_options'}, function(response) {
 		if (response) {
 			_this.option.setRawStorageData(response.options_json);
 
@@ -39,56 +39,56 @@ ContentScripts.prototype.createInfoDiv = function () {
 
 	if ( ! this.commandDiv) {
 		this.commandDiv = document.createElement('div');
-		this.commandDiv.id = "gestureCommandDiv";
+		this.commandDiv.id = 'gestureCommandDiv';
 	}
 
 	if ( ! this.actionNameDiv) {
 		this.actionNameDiv = document.createElement('div');
-		this.actionNameDiv.id = "gestureActionNameDiv";
+		this.actionNameDiv.id = 'gestureActionNameDiv';
 	}
 
 	if ( ! this.infoDiv) {
 		this.infoDiv = document.createElement('div');
-		this.infoDiv.id = "infoDiv";
+		this.infoDiv.id = 'infoDiv';
 
 		this.infoDiv.appendChild(this.commandDiv);
 		this.infoDiv.appendChild(this.actionNameDiv);
 	}
 
-	const set_width  = 300;
-	const set_height = 80;
+	const setWidth  = 300;
+	const setHeight = 80;
 
 	// style setting.
-	this.infoDiv.style.width    = set_width + "px";
-	this.infoDiv.style.height   = set_height + "px";
+	this.infoDiv.style.width    = setWidth + 'px';
+	this.infoDiv.style.height   = setHeight + 'px';
 
 	// center position.
-	this.infoDiv.style.top      = "0px";
-	this.infoDiv.style.left     = "0px";
-	this.infoDiv.style.right    = "0px";
-	this.infoDiv.style.bottom    = "0px";
-	this.infoDiv.style.margin   = "auto";
+	this.infoDiv.style.top      = '0px';
+	this.infoDiv.style.left     = '0px';
+	this.infoDiv.style.right    = '0px';
+	this.infoDiv.style.bottom    = '0px';
+	this.infoDiv.style.margin   = 'auto';
 	this.infoDiv.style.position = 'fixed';
 
-//	this.infoDiv.style.borderRadius = "3px";
-//	this.infoDiv.style.backgroundColor = "#FFFFEE";
+//	this.infoDiv.style.borderRadius = '3px';
+//	this.infoDiv.style.backgroundColor = '#FFFFEE';
 
 //	this.infoDiv.style.overflow = 'visible';
 //	this.infoDiv.style.overflow = 'block';
-	this.infoDiv.style.textAlign = "center";
+	this.infoDiv.style.textAlign = 'center';
 	this.infoDiv.style.background = 'transparent';
-	this.infoDiv.style.zIndex   ="10001";
+	this.infoDiv.style.zIndex   ='10001';
 
 	this.infoDiv.style.fontFamily = 'Arial';
-	this.infoDiv.style.fontSize = 30 + "px";
+	this.infoDiv.style.fontSize = '30px';
 	this.infoDiv.style.color      = this.option.getColorCode();
-	this.infoDiv.style.fontWeight = "bold";
+	this.infoDiv.style.fontWeight = 'bold';
 };
 
 /**
  *
  */
-ContentScripts.prototype.draw = function (lineParam, command_name, action_name) {
+ContentScripts.prototype.draw = function (lineParam, commandName, actionName) {
 	if (this.option.isTrailOn()) {
 		// append されているか調べる。document.getElementById で取得出来たらOK
 		const canvasId = this.trailCanvas.getCanvasId();
@@ -100,10 +100,10 @@ ContentScripts.prototype.draw = function (lineParam, command_name, action_name) 
 	if (this.infoDiv) {
 		if (document.getElementById(this.infoDiv.id)) {
 			const $divAction = $('#'+this.actionNameDiv.id);
-			if (this.option.isActionTextOn()) {
 
-				if (action_name != $divAction.html()) {
-					$divAction.html( (action_name != null) ? action_name : "");
+			if (this.option.isActionTextOn()) {
+				if (actionName !== $divAction.html()) {
+					$divAction.html( (actionName != null) ? actionName : "");
 				}
 			} else {
 				$divAction.html('');
@@ -111,10 +111,10 @@ ContentScripts.prototype.draw = function (lineParam, command_name, action_name) 
 
 			const $divCommand = $('#'+this.commandDiv.id);
 			if (this.option.isCommandTextOn()) {
-				command_name = this.replaceCommandToArrow(command_name);
+				commandName = this.replaceCommandToArrow(commandName);
 
-				if (command_name != $divCommand.html()) {
-					$divCommand.html(command_name);
+				if (commandName !== $divCommand.html()) {
+					$divCommand.html(commandName);
 				}
 			} else {
 				$divCommand.html('');
@@ -126,29 +126,29 @@ ContentScripts.prototype.draw = function (lineParam, command_name, action_name) 
 /**
  * Run the selected action.
  *
- * @param {type} action_name
+ * @param {type} actionName
  * @returns {undefined}
  */
-ContentScripts.prototype.exeAction = function (action_name) {
+ContentScripts.prototype.exeAction = function (actionName) {
 
-	switch (action_name) {
-		case "back":
+	switch (actionName) {
+		case 'back':
 			window.history.back();
 			break;
 
-		case "forward":
+		case 'forward':
 			window.history.forward();
 			break;
 
-		case "stop":
+		case 'stop':
 			window.stop();
 			break;
 
-		case "scroll_top":
+		case 'scroll_top':
 			window.scrollTo(0, 0);
 			break;
 
-		case "scroll_bottom":
+		case 'scroll_bottom':
 			window.scrollTo(0, $(document).height());
 			break;
 
@@ -160,16 +160,16 @@ ContentScripts.prototype.exeAction = function (action_name) {
 
 /**
  * ジェスチャコマンドを矢印表記に変換して返す D=>↓、U=>↑...
- * @param {string} action_name
+ * @param {string} actionName
  * @returns {string}
  */
-ContentScripts.prototype.replaceCommandToArrow = function (action_name) {
-	if (action_name) {
-		action_name = action_name.replace(/U/g, '<i class="flaticon-up-arrow"></i>');
-		action_name = action_name.replace(/L/g, '<i class="flaticon-left-arrow"></i>');
-		action_name = action_name.replace(/R/g, '<i class="flaticon-right-arrow"></i>');
-		action_name = action_name.replace(/D/g, '<i class="flaticon-down-arrow"></i>');
+ContentScripts.prototype.replaceCommandToArrow = function (actionName) {
+	if (actionName) {
+		actionName = actionName.replace(/U/g, '<i class="flaticon-up-arrow"></i>');
+		actionName = actionName.replace(/L/g, '<i class="flaticon-left-arrow"></i>');
+		actionName = actionName.replace(/R/g, '<i class="flaticon-right-arrow"></i>');
+		actionName = actionName.replace(/D/g, '<i class="flaticon-down-arrow"></i>');
 	}
 
-	return action_name;
+	return actionName;
 };
