d724eb122b028656d950e1fb4314a96992620e27
RyutaKojima <rkojima0213@gmail.com>
2020-12-22 18:58:00 +0900

    Linuxでも動くように調整
    検証ずみ環境
    - Raspbian GNU/Linux 9 x Chromium 72.0.3626.121

--- src/js/app/content/handler.ts
+++ src/js/app/content/handler.ts
@@ -174,7 +174,7 @@ const scrollLeft = (): number => (document.documentElement && document.documentE
    */
   document.addEventListener('contextmenu', (event: MouseEvent) => {
     const bowser = Bowser.getParser(window.navigator.userAgent)
-    if (bowser.isOS('macOS')) {
+    if (bowser.isOS('macOS') || bowser.isOS('Linux')) {
       if (timerId) {
         clearTimeout(timerId)
         timerId = null
