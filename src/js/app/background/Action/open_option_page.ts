29e1a3c41484b9da6960a405f3f48f9e6834741e
r-kojima <kojima@sailboat.co.jp>
2023-03-06 20:13:54 +0900

    fix file path

--- src/js/app/background/Action/open_option_page.ts
+++ src/js/app/background/Action/open_option_page.ts
@@ -1,7 +1,7 @@
 import {chromeTabs} from '../chrome-wrapper/chromeTabs';
 
 export default async (): Promise<void> => {
-  const optionPageUrl: string = chrome.runtime.getURL('html/options_page.html');
+  const optionPageUrl: string = chrome.runtime.getURL('options_page/options_page.html');
 
   await chromeTabs.activateOrCreate(optionPageUrl);
 };
